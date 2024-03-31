358a93d693039e16ac99a68efa27f2c6969cfb0e
brmntyprytm <bramantyopriyou@gmail.com>
2024-03-31 08:25:32 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/heymart/HeyMartApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.heymart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyMartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeyMartApplication.class, args);
+    }
+
+}
