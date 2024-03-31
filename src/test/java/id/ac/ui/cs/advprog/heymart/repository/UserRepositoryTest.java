8ddeba34e0fb11d3b8ab72137640bb60df167caa
brmntyprytm <bramantyopriyou@gmail.com>
2024-03-31 10:15:22 +0700

    [RED]: Added tests for UserRepository

---
+++ src/test/java/id/ac/ui/cs/advprog/heymart/repository/UserRepositoryTest.java
@@ -0,0 +1,29 @@
+package id.ac.ui.cs.advprog.heymart.repository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import id.ac.ui.cs.advprog.heymart.model.User;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void testSaveUser() {
+        User user = new User(null, "test_user", "test_password", "test@example.com");
+        User savedUser = userRepository.save(user);
+        assertNotNull(savedUser.getId());
+
+        User retrievedUser = userRepository.findById(savedUser.getId());
+        assertNotNull(retrievedUser);
+        assertEquals(savedUser.getId(), retrievedUser.getId());
+        assertEquals("test_user", retrievedUser.getUsername());
+        assertEquals("test_password", retrievedUser.getPassword());
+        assertEquals("test@example.com", retrievedUser.getEmail());
+    }
+}
