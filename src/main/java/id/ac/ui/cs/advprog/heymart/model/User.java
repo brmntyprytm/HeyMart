1f2456c425d5eb8f774f80519baa84d6847e42bf
brmntyprytm <bramantyopriyou@gmail.com>
2024-03-31 09:39:39 +0700

    [RED]: Added User model skeleton

--- src/main/java/id/ac/ui/cs/advprog/heymart/model/User.java
+++ src/main/java/id/ac/ui/cs/advprog/heymart/model/User.java
@@ -1,4 +1,27 @@
 package id.ac.ui.cs.advprog.heymart.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class User {
+
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password, String email) {
+        // TODO: Implement constructor
+    }
+
+    @Override
+    public String toString() {
+        // TODO: Implement toString
+        return null;
+    }
 }
