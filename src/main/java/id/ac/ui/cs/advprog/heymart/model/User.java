c6e2e0cc05acc400dee0bc5e55be9ad65db6af55
brmntyprytm <bramantyopriyou@gmail.com>
2024-03-31 09:38:51 +0700

    [RED]: Added tests for User model

---
+++ src/main/java/id/ac/ui/cs/advprog/heymart/model/User.java
@@ -0,0 +1,4 @@
+package id.ac.ui.cs.advprog.heymart.model;
+
+public class User {
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/heymart/model/UserModelTest.java
@@ -0,0 +1,25 @@
+package id.ac.ui.cs.advprog.heymart.model;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserModelTest {
+
+    @Test
+    void testUserConstructorAndGetters() {
+        User user = new User(1L, "john_doe", "password123", "john.doe@example.com");
+
+        assertEquals(1L, user.getId());
+        assertEquals("john_doe", user.getUsername());
+        assertEquals("password123", user.getPassword());
+        assertEquals("john.doe@example.com", user.getEmail());
+    }
+
+    @Test
+    void testUserToString() {
+        User user = new User(1L, "john_doe", "password123", "john.doe@example.com");
+
+        assertNotNull(user.toString());
+    }
+}
+
