eb0b392041c82e24b4f1f9e822292a0fb86295c5
brmntyprytm <bramantyopriyou@gmail.com>
2024-05-02 18:44:03 +0700

    [REFACTOR]: Refactored register and login controllers for validation

--- src/main/java/id/ac/ui/cs/advprog/heymart/controller/UserController.java
+++ src/main/java/id/ac/ui/cs/advprog/heymart/controller/UserController.java
@@ -2,9 +2,12 @@ package id.ac.ui.cs.advprog.heymart.controller;
 
 import id.ac.ui.cs.advprog.heymart.model.User;
 import id.ac.ui.cs.advprog.heymart.service.UserService;
+import id.ac.ui.cs.advprog.heymart.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -13,6 +16,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private UserValidator userValidator;
 
     @GetMapping("/")
     public String landingPage() {
@@ -31,11 +37,18 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public String registerUser(@ModelAttribute User user, Model model) {
+    public String registerUser(@ModelAttribute @Validated User user, BindingResult bindingResult, Model model) {
+        userValidator.validate(user, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+
         boolean isRegistered = userService.registerUser(user);
         if (isRegistered) {
+            model.addAttribute("success", true);
             return "redirect:/login";
         } else {
+            model.addAttribute("error", true);
             model.addAttribute("message", "Registration failed. Please try again.");
             return "register";
         }
