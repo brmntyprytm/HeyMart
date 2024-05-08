aa9a53c87a94b4d72f13dab9d606a5800d9d3dd9
Bramantyo Priyo Utomo <bramantyo.priyo@ui.ac.id>
2024-05-08 17:52:19 +0700

    [CHORE]: Deleted grocery model, services, and repository

--- src/main/java/id/ac/ui/cs/advprog/heymart/repository/GroceryRepository.java
+++
@@ -1,9 +0,0 @@
-package id.ac.ui.cs.advprog.heymart.repository;
-
-import id.ac.ui.cs.advprog.heymart.model.Grocery;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroceryRepository extends JpaRepository<Grocery, Long> {
-    Grocery findByName(String name);
-    boolean existsByName(String name);
-}
