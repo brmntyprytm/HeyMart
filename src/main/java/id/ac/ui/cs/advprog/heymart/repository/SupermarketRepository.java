223b279eb513b804f10db70e8fc3fec18db2c451
Pascal Pahlevi <PascalPahlevi@gmail.com>
2024-05-26 15:49:49 +0700

    [Refactor] Reimplemented feature

--- src/main/java/id/ac/ui/cs/advprog/heymart/repository/SupermarketRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/heymart/repository/SupermarketRepository.java
@@ -1,7 +1,18 @@
 package id.ac.ui.cs.advprog.heymart.repository;
 
 import id.ac.ui.cs.advprog.heymart.model.Supermarket;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SupermarketRepository extends JpaRepository<Supermarket, Long> {
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Supermarket s WHERE s.id = :id")
+    void deleteSupermarketById(@Param("id")Long id);
 }
