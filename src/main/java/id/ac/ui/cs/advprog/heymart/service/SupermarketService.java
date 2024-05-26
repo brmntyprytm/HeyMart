9cbc249fb31c0fbddb4b93e9fccd9c263495ec07
Pascal Pahlevi <PascalPahlevi@gmail.com>
2024-05-26 22:01:46 +0700

    [FIX] Fixed minor issue

--- src/main/java/id/ac/ui/cs/advprog/heymart/service/SupermarketService.java
+++ src/main/java/id/ac/ui/cs/advprog/heymart/service/SupermarketService.java
@@ -1,9 +1,11 @@
 package id.ac.ui.cs.advprog.heymart.service;
 
 import id.ac.ui.cs.advprog.heymart.model.Supermarket;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface SupermarketService {
 
     List<Supermarket> getAllSupermarkets();
